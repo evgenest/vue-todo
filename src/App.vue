97faa6d0d9ce5f48f2413985019fad397cddc37a
Evgenii Nestor <evgenest@onmail.com>
2023-06-23 09:03:39 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo" autofocus placeholder="What needs to be done?" @keyup.enter="addTodo">
+    </header>
+    <ul class="todo-list">
+      <li v-for="todo of todos" class="todo">{{ todo }}</li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+let todos = ref([1, 2, 3]);
+</script>
+
+<style>
+@import url('https://unpkg.com/todomvc-app-css@2.4.2/index.css');
+</style>
