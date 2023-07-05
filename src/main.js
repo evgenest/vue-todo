5b771c4b5ccd6ed36dcf913589c6ad142cef8d8c
Evgenii Nestor <evgenest@onmail.com>
2023-07-05 15:27:04 +0200

    fix css imports and remove pinia

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import './assets/main.css'
+// import css styles
+// import './assets/main.css'
+import '@node_modules/todomvc-app-css/index.css'
 
+// create the vue app
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
