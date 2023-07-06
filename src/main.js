fc71bdbadef0e11bcd9b7e3c78f7091ac681e8ab
Evgenii Nestor <evgenest@onmail.com>
2023-07-06 09:37:23 +0200

    fix husky config and restore pinia

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import '@node_modules/todomvc-app-css/index.css'
 
 // create the vue app
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
